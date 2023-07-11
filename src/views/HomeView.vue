107db6dfc226c34abc3ef6c80d88473041a221cd
unknown <berred.yassine@gmail.com>
2023-07-11 23:19:35 +0200

    update landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
         }">
             <v-btn color="white" size="small" variant="elevated" class="ml-2">
                 <div class="text-subtitle-1 font-weight-black ">
-                    تسجيل الدخول
+                    الدخول
                     <v-icon class="ml-1 ">mdi-lock-outline</v-icon>
                 </div>
             </v-btn>
@@ -24,16 +24,16 @@
             <v-row class="d-flex justify-center align-center flex-row-reverse"
                 style="padding-top: 50px;padding-bottom: 50px;">
                 <v-col class="text-center" cols="12" md="6">
-                    <div class=" text-h4  text-right  font-weight-black mb-5"> <v-icon
+                    <div class=" text-h4 font-weight-black mb-5"> <v-icon
                             class="text-green">mdi-whatsapp</v-icon> أنشئ
                         متجرك
                         المرتبط بالواتساب</div>
-                    <div class="text-h5 text-right mt-3 mb-5">
+                    <div class="text-h5 mt-3 mb-5">
                         يمكنك الآن إستلام و تأكيد طلباتك عبر الواتساب بكل سهولة
                     </div>
                     <router-link class="text-decoration-none" to="signup">
-                        <v-btn rounded="pill" color="white" size="x-large" variant="elevated" class="mt-5">
-                            <div class="text-h5 text-green-darken-3 font-weight-black ">
+                        <v-btn rounded="pill" color="green-darken-3 " size="x-large" variant="elevated" class="mt-5">
+                            <div class="text-h5 text-white font-weight-black ">
                                 أنشئ متجرك
                                 <v-icon class="ml-2 ">mdi-arrow-right-circle</v-icon>
                             </div>
@@ -49,7 +49,7 @@
             <v-col class="text-right" cols="12" md="6">
                 <div class=" text-h4 font-weight-black mb-5">جميع طلباتك منظمة </div>
                 <div class="text-h5 mt-3 mb-5">
-                    لاحاجة للبحث في رسائل الواتساب على طلباتك بعد الآن، كل طلباتك منظمة و يمكن تأكيدها بزر واحد
+                    لاحاجة للبحث في رسائل الواتساب على طلباتك بعد الآن، كل طلباتك منظمة في مكان واحد
                 </div>
             </v-col>
             <v-col>
@@ -60,7 +60,7 @@
             <v-col class="text-right" cols="12" md="6">
                 <div class=" text-h4 font-weight-black mb-5"> أنشئ متجرك الإلكتروني في دقائق</div>
                 <div class="text-h5 mt-3 mb-5">
-                    أضف المنتجات و المعلومات الخاصة بمتجرك
+                     أضف المنتجات و المعلومات الخاصة بمتجرك
                 </div>
             </v-col>
             <v-col>
@@ -70,31 +70,29 @@
         <div class=" text-center text-h4 font-weight-black mt-5 mb-5">الأسعار</div>
         <v-row class="d-flex justify-center flex-row-reverse ">
             <v-col class="d-flex justify-center mt-5" :cols="$vuetify.display.mdAndUp ? '4' : '12'">
-                <v-card width="100%" elevation="2">
+                <v-card :width="$vuetify.display.mdAndUp ? '80%' : '100%'" color="" elevation="2">
                     <v-card-title class=" text-center text-h5 font-weight-black mt-2">باقة إنطلاقة</v-card-title>
                     <v-card-title class=" text-center text-h6 font-weight-black text-green-darken-1 mb-5">0 DH /
                         سنوي</v-card-title>
-                    <v-list class="text-right font-weight-black text-subtitle-1" lines="one">
-                        <v-list-item append-icon="mdi-check-circle" class="text-green-darken-1">
-                            <span class="text-black">
-                                متجر إلكتروني مرتبط بالواتساب
-                            </span>
-                        </v-list-item>
-                        <v-list-item append-icon="mdi-check-circle" class="text-green-darken-1">
-                            <span class="text-black">
-                                عدد لا محدود من الطلبات
-                            </span>
-                        </v-list-item>
-                        <v-list-item append-icon="mdi-check-circle" class="text-green-darken-1">
-                            <span class="text-black">
-                                عدد لا محدود من المنتجات
-                            </span>
-                        </v-list-item>
-                        <v-list-item  class="text-green-darken-1">
-                            <span class="text-black">
-                            </span>
-                        </v-list-item>
-                    </v-list>
+                    <v-list-item append-icon="mdi-check-circle" class="text-right text-green-darken-1">
+                        <span class="font-weight-black text-subtitle-1 text-black">
+                            متجر إلكتروني مرتبط بالواتساب
+                        </span>
+                    </v-list-item>
+                    <v-list-item append-icon="mdi-check-circle" class="text-right text-green-darken-1">
+                        <span class="font-weight-black text-subtitle-1 text-black">
+                            عدد لا محدود من الطلبات
+                        </span>
+                    </v-list-item>
+                    <v-list-item append-icon="mdi-check-circle" class="text-right text-green-darken-1">
+                        <span class="font-weight-black text-subtitle-1 text-black">
+                            عدد لا محدود من المنتجات
+                        </span>
+                    </v-list-item>
+                    <v-list-item class="text-green-darken-1">
+                        <span class="font-weight-black text-subtitle-1 text-black">
+                        </span>
+                    </v-list-item>
                     <router-link class="text-decoration-none" to="signup">
                         <v-card-actions class="d-flex justify-center mb-2 mt-2">
                             <v-btn rounded="pill" color="green-darken-1" size="x-large" variant="elevated" class="mt-5">
@@ -106,42 +104,41 @@
                         </v-card-actions>
                     </router-link>
                 </v-card>
-            </v-col><v-col class="d-flex justify-center mt-5" :cols="$vuetify.display.mdAndUp ? '4' : '12'" >
-                <v-card width="100%" class="" elevation="2">
+            </v-col>
+            <v-col class="d-flex justify-center mt-5" :cols="$vuetify.display.mdAndUp ? '4' : '12'">
+                <v-card :width="$vuetify.display.mdAndUp ? '80%' : '100%'" color="green-lighten-5" elevation="2">
                     <v-card-title class=" text-center text-h5 font-weight-black mt-2">باقة محترف</v-card-title>
-                    <v-card-title class=" text-center text-h6 font-weight-black text-green-darken-1 mb-5">980 DH /
+                    <v-card-title class=" text-center text-h6 font-weight-black text-green-darken-1 mb-5">950 DH /
                         سنوي</v-card-title>
-                    <v-list class="text-right font-weight-black text-subtitle-1" lines="one">
-                        <v-list-item append-icon="mdi-check-circle" class="text-green-darken-1">
-                            <span class="text-black">كل مميزات باقة إنطلاقة
-                            </span>
-                        </v-list-item>
-                        <v-list-item append-icon="mdi-check-circle" class="text-green-darken-1">
-                            <span class="text-black">
-                                تصميم موقع إحترافي
-                            </span>
-                        </v-list-item>
-                        <v-list-item append-icon="mdi-check-circle" class="text-green-darken-1">
-                            <span class="text-black">
-                                عنوان موقع ويب
-                            </span>
-                        </v-list-item>
-                        <v-list-item append-icon="mdi-check-circle" class="text-green-darken-1">
-                            <span class="text-black">
-                                متجر متعدد اللغات
-                            </span>
-                        </v-list-item>
-                    </v-list>
-                    <router-link class="text-decoration-none" to="signup">
+                    <v-list-item append-icon="mdi-check-circle" class="text-right text-green-darken-1">
+                        <span class="font-weight-black text-subtitle-1 text-black">كل مميزات باقة إنطلاقة
+                        </span>
+                    </v-list-item>
+                    <v-list-item append-icon="mdi-check-circle" class="text-right text-green-darken-1">
+                        <span class="font-weight-black text-subtitle-1 text-black">
+                            تصميم موقع إحترافي
+                        </span>
+                    </v-list-item>
+                    <v-list-item append-icon="mdi-check-circle" class="text-right text-green-darken-1">
+                        <span class="font-weight-black text-subtitle-1 text-black">
+                            عنوان موقع ويب
+                        </span>
+                    </v-list-item>
+                    <v-list-item append-icon="mdi-check-circle" class="text-right text-green-darken-1">
+                        <span class="font-weight-black text-subtitle-1 text-black">
+                            متجر متعدد اللغات
+                        </span>
+                    </v-list-item>
                         <v-card-actions class="d-flex justify-center mb-2 mt-2">
-                            <v-btn rounded="pill" color="green-darken-1" size="x-large" variant="elevated" class="mt-5">
-                                <div class="text-subtitle-1 ml-2 mr-2 text-white font-weight-black ">
-                                    تواصل  معنا
-                                    <v-icon class="ml-2 ">mdi-whatsapp</v-icon>
-                                </div>
-                            </v-btn>
+                            <a class="text-decoration-none text-white" target="_blank" href="https://wa.me/+212673090675">
+                                <v-btn rounded="pill" color="green-darken-1" size="x-large" variant="elevated" class="mt-5">
+                                    <div class="text-subtitle-1 ml-2 mr-2 text-white font-weight-black ">
+                                        تواصل معنا
+                                        <v-icon class="ml-2 ">mdi-whatsapp</v-icon>
+                                    </div>
+                                </v-btn>
+                            </a>
                         </v-card-actions>
-                    </router-link>
                 </v-card>
             </v-col>
         </v-row>
@@ -186,6 +183,7 @@ export default {
 .hero {
     padding-left: 50px;
     padding-right: 50px;
+    padding-bottom: 50px;
 }
 
 .text-h3 {
@@ -210,10 +208,4 @@ export default {
 .text-subtitle-1 {
     font-family: $body-font-family !important;
 }
-
-.header-btn {
-    background-image: linear-gradient(to top right, rgb(68, 199, 116), rgb(68, 199, 116));
-}
-
-
 </style>
